fc472613051a4c741855ac8bf63224f9916c0a2f
rfsheffer <rfsheffer@gmail.com>
2020-05-11 14:21:36 -0700

    - Removed Convex collision generator call. I found epics version. Added a couple other useful functions for controlling static meshes from utility blueprints.

--- Source/RyEditor/Private/RyEditorStaticMeshHelpers.cpp
+++ Source/RyEditor/Private/RyEditorStaticMeshHelpers.cpp
@@ -9,104 +9,46 @@
 #include "EditorViewportCommands.h"
 #include "Editor/UnrealEd/Private/ConvexDecompTool.h"
 #include "PhysicsEngine/BodySetup.h"
+#include "Subsystems/AssetEditorSubsystem.h"
 
 // Set USE_ASYNC_DECOMP to zero to go back to the fully synchronous; blocking version of V-HACD
 // #ifndef USE_ASYNC_DECOMP
 // #define USE_ASYNC_DECOMP 1
 // #endif
 
-//---------------------------------------------------------------------------------------------------------------------
-/**
-*/
-void URyEditorStaticMeshHelpers::ApplyConvexDecomposition(UStaticMesh* StaticMesh, int32 InHullCount /*= 4*/, int32 InMaxHullVerts /*= 16*/, int32 InHullPrecision /*= 100000*/)
+void URyEditorStaticMeshHelpers::SetCollisionTraceFlag(class UStaticMesh* StaticMesh, TEnumAsByte<enum ECollisionTraceFlag> collisionTraceFlag)
 {
-    // Check we have a selected StaticMesh
-    if(StaticMesh && StaticMesh->RenderData && InHullCount > 0 && InMaxHullVerts > 0 && InHullPrecision > 0)
+    if(StaticMesh && StaticMesh->BodySetup)
     {
-        FStaticMeshLODResources& LODModel = StaticMesh->RenderData->LODResources[0];
-
-        // Start a busy cursor so the user has feedback while waiting
-        const FScopedBusyCursor BusyCursor;
-
-        // Make vertex buffer
-        int32 NumVerts = LODModel.VertexBuffers.StaticMeshVertexBuffer.GetNumVertices();
-        TArray<FVector> Verts;
-        for(int32 i = 0; i < NumVerts; i++)
+        UAssetEditorSubsystem* AssetEditorSubsystem = GEditor->GetEditorSubsystem<UAssetEditorSubsystem>();
+        bool bStaticMeshIsEdited = false;
+        if(AssetEditorSubsystem->FindEditorForAsset(StaticMesh, false))
         {
-            FVector Vert = LODModel.VertexBuffers.PositionVertexBuffer.VertexPosition(i);
-            Verts.Add(Vert);
+            AssetEditorSubsystem->CloseAllEditorsForAsset(StaticMesh);
+            bStaticMeshIsEdited = true;
         }
 
-        // Grab all indices
-        TArray<uint32> AllIndices;
-        LODModel.IndexBuffer.GetCopy(AllIndices);
+        StaticMesh->BodySetup->CollisionTraceFlag = collisionTraceFlag;
+        StaticMesh->BodySetup->Modify();
 
-        // Only copy indices that have collision enabled
-        TArray<uint32> CollidingIndices;
-        for(const FStaticMeshSection& Section : LODModel.Sections)
-        {
-            if(Section.bEnableCollision)
-            {
-                for(uint32 IndexIdx = Section.FirstIndex; IndexIdx < Section.FirstIndex + (Section.NumTriangles * 3); IndexIdx++)
-                {
-                    CollidingIndices.Add(AllIndices[IndexIdx]);
-                }
-            }
-        }
-
-        //ClearSelectedPrims();
-
-        // Make sure rendering is done - so we are not changing data being used by collision drawing.
-        FlushRenderingCommands();
-
-        // Get the BodySetup we are going to put the collision into
-        UBodySetup* bs = StaticMesh->BodySetup;
-        if(bs)
-        {
-            bs->RemoveSimpleCollision();
-        }
-        else
-        {
-            // Otherwise, create one here.
-            StaticMesh->CreateBodySetup();
-            bs = StaticMesh->BodySetup;
-        }
-
-        // Run actual util to do the work (if we have some valid input)
-        if(Verts.Num() >= 3 && CollidingIndices.Num() >= 3)
-        {
-            // RY NOTE: I don't think this should be async unless I do some leg work to ensure the user cannot create some race condition.
-// #if USE_ASYNC_DECOMP
-//             // If there is currently a decomposition already in progress we release it.
-//             if(DecomposeMeshToHullsAsync)
-//             {
-//                 DecomposeMeshToHullsAsync->Release();
-//             }
-//             // Begin the convex decomposition process asynchronously
-//             DecomposeMeshToHullsAsync = CreateIDecomposeMeshToHullAsync();
-//             DecomposeMeshToHullsAsync->DecomposeMeshToHullsAsyncBegin(bs, Verts, CollidingIndices, InHullCount, InMaxHullVerts, InHullPrecision);
-// #else
-//             DecomposeMeshToHulls(bs, Verts, CollidingIndices, InHullCount, InMaxHullVerts, InHullPrecision);
-// #endif
-
-            DecomposeMeshToHulls(bs, Verts, CollidingIndices, InHullCount, InMaxHullVerts, InHullPrecision);
-        }
-
-        // Enable collision, if not already
-//         if(!Viewport->GetViewportClient().IsShowSimpleCollisionChecked())
-//         {
-//             Viewport->GetViewportClient().ToggleShowSimpleCollision();
-//         }
-
-        // refresh collision change back to staticmesh components
+        // refresh collision change back to static mesh components
         RefreshCollisionChange(*StaticMesh);
 
-        // Mark mesh as dirty
-        StaticMesh->MarkPackageDirty();
+        // Request re-building of mesh with new collision shapes
+        StaticMesh->PostEditChange();
 
-        // Update screen.
-        //Viewport->RefreshViewport();
+        // Reopen MeshEditor on this mesh if the MeshEditor was previously opened in it
+        if(bStaticMeshIsEdited)
+        {
+            AssetEditorSubsystem->OpenEditorForAsset(StaticMesh);
+        }
+    }
+}
 
-        StaticMesh->bCustomizedCollision = true;	//mark the static mesh for collision customization
+void URyEditorStaticMeshHelpers::SetCanAffectNavMesh(class UStaticMeshComponent* StaticMesh, bool canAffect)
+{
+    if(StaticMesh)
+    {
+        StaticMesh->SetCanEverAffectNavigation(canAffect);
     }
 }
--- Source/RyEditor/Public/RyEditorStaticMeshHelpers.h
+++ Source/RyEditor/Public/RyEditorStaticMeshHelpers.h
@@ -2,6 +2,8 @@
 
 #pragma once
 
+#include "PhysicsEngine/BodySetupEnums.h"
+
 #include "RyEditorStaticMeshHelpers.generated.h"
 
 //---------------------------------------------------------------------------------------------------------------------
@@ -14,8 +16,9 @@ class RYEDITOR_API URyEditorStaticMeshHelpers : public UBlueprintFunctionLibrary
     GENERATED_BODY()
 public:
 
-    // Apply Convex Decomposition to a mesh. This overrides the meshes simple collision.
-    // This is useful for cases where you want to apply auto generated collision to a number of meshes.
     UFUNCTION(BlueprintCallable, CallInEditor, Category = "RyEditor|AnimationHelpers")
-    static void ApplyConvexDecomposition(class UStaticMesh* StaticMesh, int32 InHullCount = 4, int32 InMaxHullVerts = 16, int32 InHullPrecision = 100000);
+    static void SetCollisionTraceFlag(class UStaticMesh* StaticMesh, TEnumAsByte<enum ECollisionTraceFlag> collisionTraceFlag);
+
+    UFUNCTION(BlueprintCallable, CallInEditor, Category = "RyEditor|AnimationHelpers")
+    static void SetCanAffectNavMesh(class UStaticMeshComponent* StaticMesh, bool canAffect);
 };
