73a53fa92d8a5baf05e135c9aa4527b157a9b75a
rfsheffer <rfsheffer@gmail.com>
2020-05-24 18:04:53 -0700

    - SoundWave helpers for setting subtitles via blueprints

---
+++ Source/RyEditor/Private/RyEditorSoundWaveHelpers.cpp
@@ -0,0 +1,33 @@
+// Copyright 2020 Sheffer Online Services. All Rights Reserved.
+
+#include "RyEditorSoundWaveHelpers.h"
+#include "Sound/SoundWave.h"
+
+//---------------------------------------------------------------------------------------------------------------------
+/**
+*/
+FString URyEditorSoundWaveHelpers::GetSpokenText(USoundWave* soundWave)
+{
+    if(!soundWave)
+        return TEXT("");
+
+    return soundWave->SpokenText;
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+/**
+*/
+void URyEditorSoundWaveHelpers::SetSubtitles(USoundWave* soundWave, const TArray<FRySubtitleCue>& subtitles)
+{
+    if(!soundWave)
+        return;
+
+    soundWave->Subtitles.Empty();
+    for(const FRySubtitleCue& cue : subtitles)
+    {
+        FSubtitleCue cueIn;
+        cueIn.Text = cue.Text;
+        cueIn.Time = cue.Time;
+        soundWave->Subtitles.Add(cueIn);
+    }
+}
---
+++ Source/RyEditor/Public/RyEditorSoundWaveHelpers.h
@@ -0,0 +1,43 @@
+// Copyright 2020 Sheffer Online Services. All Rights Reserved.
+
+#pragma once
+
+#include "RyEditorSoundWaveHelpers.generated.h"
+
+USTRUCT(BlueprintType)
+struct FRySubtitleCue
+{
+    GENERATED_BODY()
+
+    /** The text to appear in the subtitle. */
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = SubtitleCue)
+    FText Text;
+
+    /** The time at which the subtitle is to be displayed, in seconds relative to the beginning of the line. */
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = SubtitleCue)
+    float Time;
+
+    FRySubtitleCue()
+        : Time(0)
+    {
+    }
+};
+
+//---------------------------------------------------------------------------------------------------------------------
+/**
+  * Static Helper functions related to SoundWave assets
+*/
+UCLASS()
+class RYEDITOR_API URyEditorSoundWaveHelpers : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+public:
+
+    /** A localized version of the text that is actually spoken phonetically in the audio. */
+    UFUNCTION(BlueprintPure, CallInEditor, Category = "RyEditor|SoundWaveHelpers")
+    static FString GetSpokenText(class USoundWave* soundWave);
+
+    /** Override the current subtitles in a SoundWave asset */
+    UFUNCTION(BlueprintCallable, CallInEditor, Category = "RyEditor|SoundWaveHelpers")
+    static void SetSubtitles(class USoundWave* soundWave, const TArray<FRySubtitleCue>& subtitles);
+};
