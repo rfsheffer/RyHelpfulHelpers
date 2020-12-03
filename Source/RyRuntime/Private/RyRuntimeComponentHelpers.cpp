8f0f4a9ad20bebc4e60b769b3d594163091b398e
rfsheffer <rfsheffer@gmail.com>
2020-12-03 08:09:14 -0800

    - Added component helper for the SplineComponent to take an arbitrary number of points (greater than 2) and apply them to the spline with is already initialized with points. This is useful for updating a splines arc with more or less points but maintaining the splines base number of points. I am using this to pass in PredictedTrajectory points to the spline (which the number of points is variable) and using that spline to update spline mesh components without having to re-make the components each frame.

---
+++ Source/RyRuntime/Private/RyRuntimeComponentHelpers.cpp
@@ -0,0 +1,61 @@
+﻿// Copyright 2020 Sheffer Online Services.
+// MIT License. See LICENSE for details.
+
+#include "RyRuntimeComponentHelpers.h"
+#include "RyRuntimeModule.h"
+
+//---------------------------------------------------------------------------------------------------------------------
+/**
+*/
+void URyRuntimeComponentHelpers::DistributePointsToSpline(USplineComponent* splineComponent,
+														  const TArray<FVector>& points,
+														  ESplineCoordinateSpace::Type coordinateSpace,
+														  bool updateSpline)
+{
+	if(!splineComponent)
+	{
+		UE_LOG(LogRyRuntime, Warning, TEXT("DistributePointsToSpline called with null splineComponent!"));
+		return;
+	}
+
+	const int32 numSplinePoints = splineComponent->GetNumberOfSplinePoints();
+	if(numSplinePoints < 2)
+	{
+		UE_LOG(LogRyRuntime, Warning, TEXT("DistributePointsToSpline called with splineComponent with invalid number of points. An initialized with points (>=2) spline is required!"));
+		return;
+	}
+
+	if(points.Num() < 2)
+	{
+		UE_LOG(LogRyRuntime, Warning, TEXT("DistributePointsToSpline called with not enough points... Requires (>=2)"));
+		return;
+	}
+
+	TArray<FVector> distributePoints;
+	distributePoints.SetNum(numSplinePoints);
+	if(points.Num() != numSplinePoints)
+	{
+		// Always set the first and last point exactly
+		distributePoints[0] = points[0];
+		distributePoints[numSplinePoints - 1] = points[points.Num() - 1];
+
+		// Get mid values between points, works for both expansion and contraction
+		const float frac = static_cast<float>(points.Num() - 2) / (numSplinePoints - 2);
+		for(int32 pointIndex = 1; pointIndex < distributePoints.Num() - 1; ++pointIndex)
+		{
+			const int32 lowIndex = FMath::FloorToInt(pointIndex * frac);
+			const int32 highIndex = FMath::CeilToInt(pointIndex * frac);
+			distributePoints[pointIndex] = points[lowIndex] + (points[highIndex] - points[lowIndex]) / 2.0f;
+		}
+	}
+
+	for(int32 pointIndex = 0; pointIndex < distributePoints.Num(); ++pointIndex)
+	{
+		splineComponent->SetLocationAtSplinePoint(pointIndex, distributePoints[pointIndex], coordinateSpace, false);
+	}
+
+	if(updateSpline)
+	{
+		splineComponent->UpdateSpline();
+	}
+}
---
+++ Source/RyRuntime/Public/RyRuntimeComponentHelpers.h
@@ -0,0 +1,31 @@
+﻿// Copyright 2020 Sheffer Online Services.
+// MIT License. See LICENSE for details.
+
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Components/SplineComponent.h"
+
+#include "RyRuntimeComponentHelpers.generated.h"
+
+//---------------------------------------------------------------------------------------------------------------------
+/**
+* Static Helper functions related to components in general. Adding extra functionality to components that exist with the
+* Unreal Engine.
+*/
+UCLASS()
+class RYRUNTIME_API URyRuntimeComponentHelpers : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	public:
+
+	// Takes an array of points and applies them to the spline by distributing them evenly across the spline.
+	// The spline component should be pre-populated.
+	// If there are less points than splineComponent points, the points will be interpolated to fill the gap.
+	// If there are more points than splineComponent points, you will loose resolution with the spline because decimation will occur.
+	UFUNCTION(BlueprintCallable, Category = "RyRuntime|ComponentHelpers")
+    static void DistributePointsToSpline(class USplineComponent* splineComponent,
+    								     const TArray<FVector>& points,
+    								     ESplineCoordinateSpace::Type coordinateSpace,
+    								     bool updateSpline = true);
+};
