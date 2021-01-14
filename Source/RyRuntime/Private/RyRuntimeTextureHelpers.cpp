ad76a8953bfd1032580b21fb2481ad89bad622b2
Ryan <rfsheffer@gmail.com>
2021-01-13 20:35:29 -0800

    - Texture resource loading tool for ensuring or reloading a textures resource. Use with caution!

---
+++ Source/RyRuntime/Private/RyRuntimeTextureHelpers.cpp
@@ -0,0 +1,61 @@
+﻿// Copyright 2020 Sheffer Online Services.
+// MIT License. See LICENSE for details.
+
+#include "RyRuntimeTextureHelpers.h"
+#include "Runtime/Engine/Public/LatentActions.h"
+
+class FRyEnsureTextureResourceLatentAction : public FPendingLatentAction
+{
+public:
+	FName ExecutionFunction;
+	int32 OutputLink;
+	FWeakObjectPtr CallbackTarget;
+	ERyTextureResourceResult* Result = nullptr;
+	TWeakObjectPtr<UTexture> TextureIn;
+
+	FRyEnsureTextureResourceLatentAction(const FLatentActionInfo& LatentInfo, UTexture* Texture, ERyTextureResourceResult& Mode)
+        : ExecutionFunction(LatentInfo.ExecutionFunction)
+        , OutputLink(LatentInfo.Linkage)
+        , CallbackTarget(LatentInfo.CallbackTarget)
+        , Result(&Mode)
+		, TextureIn(Texture)
+	{
+		if(Texture && Texture->Resource)
+		{
+			Texture->UpdateResource();
+		}
+	}
+
+	virtual void UpdateOperation(FLatentResponse& Response) override
+	{
+		if(Response.ElapsedTime() > 20.0f || !TextureIn.IsValid(false) || !TextureIn->Resource)
+		{
+			*Result = ERyTextureResourceResult::Failure;
+			Response.FinishAndTriggerIf(true, ExecutionFunction, OutputLink, CallbackTarget);
+			return;
+		}
+		if (TextureIn->Resource && TextureIn->Resource->TextureRHI)
+		{
+			*Result = ERyTextureResourceResult::Success;
+			Response.FinishAndTriggerIf(true, ExecutionFunction, OutputLink, CallbackTarget);
+		}
+	}
+};
+
+//---------------------------------------------------------------------------------------------------------------------
+/**
+*/
+void URyRuntimeTextureHelpers::UpdateTextureResource(UObject* WorldContextObject, UTexture* Texture, FLatentActionInfo LatentInfo, ERyTextureResourceResult& Result)
+{
+	if (UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::LogAndReturnNull))
+	{
+		FLatentActionManager& LatentActionManager = World->GetLatentActionManager();
+		if (LatentActionManager.FindExistingAction<FRyEnsureTextureResourceLatentAction>(LatentInfo.CallbackTarget, LatentInfo.UUID) == nullptr)
+		{
+			Result = ERyTextureResourceResult::Failure;
+			FRyEnsureTextureResourceLatentAction* CompletionAction = new FRyEnsureTextureResourceLatentAction(LatentInfo, Texture, Result);
+
+			LatentActionManager.AddNewAction(LatentInfo.CallbackTarget, LatentInfo.UUID, CompletionAction);
+		}
+	}
+}
---
+++ Source/RyRuntime/Public/RyRuntimeTextureHelpers.h
@@ -0,0 +1,29 @@
+﻿// Copyright 2020 Sheffer Online Services.
+// MIT License. See LICENSE for details.
+
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "RyRuntimeTextureHelpers.generated.h"
+
+/** Used in blueprint latent function execution */
+UENUM(BlueprintType)
+enum class ERyTextureResourceResult : uint8
+{
+	Success,
+    Failure
+};
+
+/**
+ * 
+ */
+UCLASS()
+class RYRUNTIME_API URyRuntimeTextureHelpers : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+	// Tries to update a textures resource and returns a Success result of the resource was updated and valid.
+	// Try to pass texture objects in which are valid and could have a resource associated or this function will fail by timing out after 20 seconds.
+	UFUNCTION(BlueprintCallable, Category = "RyRuntime|TextureHelpers", meta = (WorldContext = "WorldContextObject", Latent, LatentInfo = "LatentInfo", ExpandEnumAsExecs = "Result"))
+    static void UpdateTextureResource(UObject* WorldContextObject, class UTexture* Texture, FLatentActionInfo LatentInfo, ERyTextureResourceResult& Result);
+};
