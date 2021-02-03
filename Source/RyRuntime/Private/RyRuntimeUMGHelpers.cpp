517917917c0322faac4a3e4f0b295ee0fc840c0b
Ryan <rfsheffer@gmail.com>
2021-02-02 17:45:24 -0800

    - nada

---
+++ Source/RyRuntime/Private/RyRuntimeUMGHelpers.cpp
@@ -0,0 +1,6 @@
+﻿// Copyright 2020-2021 Sheffer Online Services.
+// MIT License. See LICENSE for details.
+
+#include "RyRuntimeUMGHelpers.h"
+#include "RyRuntimeModule.h"
+#include "Components/PanelWidget.h"
---
+++ Source/RyRuntime/Public/RyRuntimeUMGHelpers.h
@@ -0,0 +1,17 @@
+﻿// Copyright 2020-2021 Sheffer Online Services.
+// MIT License. See LICENSE for details.
+
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+
+#include "RyRuntimeUMGHelpers.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RYRUNTIME_API URyRuntimeUMGHelpers : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+};
