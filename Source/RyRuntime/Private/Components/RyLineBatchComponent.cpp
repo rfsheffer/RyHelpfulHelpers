58fbb5358f8fd672bd7284c8efccd1732b0b19e9
Ryan <rfsheffer@gmail.com>
2021-04-26 19:57:51 -0700

    - Extended the LineBatchComponent to blueprint so we can create per blueprint sets of lines for debugging and not always in a global sense

---
+++ Source/RyRuntime/Private/Components/RyLineBatchComponent.cpp
@@ -0,0 +1,111 @@
+﻿// Copyright 2020-2021 Sheffer Online Services.
+// MIT License. See LICENSE for details.
+
+#include "Components/RyLineBatchComponent.h"
+
+//---------------------------------------------------------------------------------------------------------------------
+/**
+*/
+void URyLineBatchComponent::AddBox(const FBox& Box,
+											 const FTransform& BoxToWorld,
+											 const FLinearColor& Color,
+											 const float LifeTime,
+											 const ERyLineBatchDepthPriority DepthPriority)
+{
+	const float tmp = DefaultLifeTime;
+	DefaultLifeTime = LifeTime;
+	DrawBox(Box, BoxToWorld.ToMatrixWithScale(), Color.ToFColor(true), static_cast<uint8>(DepthPriority));
+	DefaultLifeTime = tmp;
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+/**
+*/
+void URyLineBatchComponent::AddDirectionalArrow(const FTransform& ArrowToWorld,
+														  const FLinearColor& Color,
+														  const float Length,
+														  const float ArrowSize,
+														  const float LifeTime,
+														  const ERyLineBatchDepthPriority DepthPriority)
+{
+	const float tmp = DefaultLifeTime;
+	DefaultLifeTime = LifeTime;
+	DrawDirectionalArrow(ArrowToWorld.ToMatrixWithScale(), Color.ToFColor(true), 
+	Length, ArrowSize, static_cast<uint8>(DepthPriority));
+	DefaultLifeTime = tmp;
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+/**
+*/
+void URyLineBatchComponent::AddCircle(const FVector& Base,
+												    const FVector& X,
+												    const FVector& Y,
+												    const FLinearColor& Color,
+												    const float Radius,
+												    const int32 NumSides,
+												    const float LifeTime,
+													const ERyLineBatchDepthPriority DepthPriority)
+{
+	const float tmp = DefaultLifeTime;
+	DefaultLifeTime = LifeTime;
+	DrawCircle(Base, X, Y, Color.ToFColor(true), Radius, NumSides, static_cast<uint8>(DepthPriority));
+	DefaultLifeTime = tmp;
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+/**
+*/
+void URyLineBatchComponent::AddLine(const FVector& Start,
+											  const FVector& End,
+											  const FLinearColor& Color,
+											  const float Thickness,
+											  const float LifeTime,
+											  const ERyLineBatchDepthPriority DepthPriority)
+{
+	DrawLine(Start, End, Color.ToFColor(true), static_cast<uint8>(DepthPriority), Thickness, LifeTime);
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+/**
+*/
+void URyLineBatchComponent::AddPoint(const FVector& Position,
+										       const FLinearColor& Color,
+										       const float PointSize,
+										       const float LifeTime,
+										       const ERyLineBatchDepthPriority DepthPriority)
+{
+	DrawPoint(Position, Color.ToFColor(true), PointSize, static_cast<uint8>(DepthPriority), LifeTime);
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+/**
+*/
+void URyLineBatchComponent::AddSolidBox(const FBox& Box,
+												  const FTransform& BoxToWorld,
+												  const FLinearColor& Color,
+												  const float LifeTime,
+												  const ERyLineBatchDepthPriority DepthPriority)
+{
+	DrawSolidBox(Box, BoxToWorld, Color.ToFColor(true), static_cast<uint8>(DepthPriority), LifeTime);
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+/**
+*/
+void URyLineBatchComponent::AddMesh(const TArray<FVector>& Verts,
+											  const TArray<int32>& Indices,
+											  const FLinearColor& Color,
+											  const float LifeTime,
+											  const ERyLineBatchDepthPriority DepthPriority)
+{
+	DrawMesh(Verts, Indices, Color.ToFColor(true), static_cast<uint8>(DepthPriority), LifeTime);
+}
+
+//---------------------------------------------------------------------------------------------------------------------
+/**
+*/
+void URyLineBatchComponent::ClearLineBatches()
+{
+	Flush();
+}
---
+++ Source/RyRuntime/Public/Components/RyLineBatchComponent.h
@@ -0,0 +1,114 @@
+﻿// Copyright 2020-2021 Sheffer Online Services.
+// MIT License. See LICENSE for details.
+
+#pragma once
+
+#include "Components/LineBatchComponent.h"
+#include "RyLineBatchComponent.generated.h"
+
+UENUM(BlueprintType)
+enum class ERyLineBatchDepthPriority : uint8
+{
+	/** Occlude vs the World scene */
+	World,
+    /** Occlude vs the Foreground scene */
+    Foreground,
+};
+
+//---------------------------------------------------------------------------------------------------------------------
+/**
+ * A component which wraps the internal LineBatchComponent
+*/
+UCLASS(ClassGroup=(Rendering), meta=(BlueprintSpawnableComponent))
+class RYRUNTIME_API URyLineBatchComponent : public ULineBatchComponent
+{
+	GENERATED_BODY()
+
+public:
+
+	/**
+	 * Add a box
+	 * @param LifeTime - The lifetime of the line. -1 means infitite. 
+	 */
+	UFUNCTION(BlueprintCallable, Category="RyLineBatch")
+	void AddBox(const FBox& Box,
+				          const FTransform& BoxToWorld,
+				          const FLinearColor& Color,
+				          const float LifeTime = 0.0f,
+				          const ERyLineBatchDepthPriority DepthPriority = ERyLineBatchDepthPriority::World);
+
+	/**
+	 * Add an arrow
+	 * @param LifeTime - The lifetime of the line. -1 means infitite. 
+	 */
+	UFUNCTION(BlueprintCallable, Category="RyLineBatch")
+	void AddDirectionalArrow(const FTransform& ArrowToWorld,
+							           const FLinearColor& Color,
+							           const float Length = 20.0f,
+							           const float ArrowSize = 20.0f,
+							           const float LifeTime = 0.0f,
+							           const ERyLineBatchDepthPriority DepthPriority = ERyLineBatchDepthPriority::World);
+
+	/**
+	 * Add a circle
+	 * @param LifeTime - The lifetime of the line. -1 means infitite. 
+	 */
+	UFUNCTION(BlueprintCallable, Category="RyLineBatch")
+	void AddCircle(const FVector& Base,
+					             const FVector& X,
+					             const FVector& Y,
+					             const FLinearColor& Color,
+					             const float Radius = 10.0f,
+					             const int32 NumSides = 20,
+					             const float LifeTime = 0.0f,
+					             const ERyLineBatchDepthPriority DepthPriority = ERyLineBatchDepthPriority::World);
+
+	/**
+	 * Add a line
+	 * @param LifeTime - The lifetime of the line. -1 means infitite. 
+	 */
+	UFUNCTION(BlueprintCallable, Category="RyLineBatch")
+	void AddLine(const FVector& Start,
+                           const FVector& End,
+                           const FLinearColor& Color,
+                           const float Thickness = 0.0f,
+                           const float LifeTime = 0.0f,
+                           const ERyLineBatchDepthPriority DepthPriority = ERyLineBatchDepthPriority::World);
+
+	/**
+	 * Add a point
+	 * @param LifeTime - The lifetime of the line. -1 means infitite. 
+	 */
+	UFUNCTION(BlueprintCallable, Category="RyLineBatch")
+	void AddPoint(const FVector& Position,
+                            const FLinearColor& Color,
+                            const float PointSize = 20.0f,
+                            const float LifeTime = 0.0f,
+                            const ERyLineBatchDepthPriority DepthPriority = ERyLineBatchDepthPriority::World);
+
+	/**
+	 * Add a solid box
+	 * @param LifeTime - The lifetime of the line. -1 means infitite. 
+	 */
+	UFUNCTION(BlueprintCallable, Category="RyLineBatch")
+	void AddSolidBox(const FBox& Box,
+					           const FTransform& BoxToWorld,
+					           const FLinearColor& Color,
+					           const float LifeTime = 0.0f,
+					           const ERyLineBatchDepthPriority DepthPriority = ERyLineBatchDepthPriority::World);
+	
+	/**
+	 * Add a mesh
+	 * @param LifeTime - The lifetime of the line. -1 means infitite. 
+	 */
+	UFUNCTION(BlueprintCallable, Category="RyLineBatch")
+	void AddMesh(const TArray<FVector>& Verts,
+				           const TArray<int32>& Indices,
+				           const FLinearColor& Color,
+				           const float LifeTime = 0.0f,
+				           const ERyLineBatchDepthPriority DepthPriority = ERyLineBatchDepthPriority::World);
+
+	/** Clear all batched lines, points and meshes */
+	UFUNCTION(BlueprintCallable, Category="RyLineBatch")
+	void ClearLineBatches();
+};
