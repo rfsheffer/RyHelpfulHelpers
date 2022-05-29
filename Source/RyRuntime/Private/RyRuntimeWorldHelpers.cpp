7f32c99057bcce86e57d564d6f31c18b06c638c7
rfsheffer <rfsheffer@gmail.com>
2022-05-29 13:11:09 -0700

    - Inspired by Oculus's utility function TickUntil, I have implemented basically the same thing but added some extra goodies. It is a latent function when executed via 'Start' will execute the 'Tick' output execution line every frame until the 'Stop' execution line is run. This is useful for momentary ticking like user input Pressed causing a per frame tick until input button Released is executed. Reduces having to write a loop with a frame delay each time.

---
+++ Source/RyRuntime/Private/RyRuntimeWorldHelpers.cpp
@@ -0,0 +1,56 @@
+﻿// Copyright 2020-2022 Sheffer Online Services.
+// MIT License. See LICENSE for details.
+
+#include "RyRuntimeWorldHelpers.h"
+
+//---------------------------------------------------------------------------------------------------------------------
+/**
+*/
+class FRyTickUntilStopAction : public FPendingLatentAction
+{
+public:
+	FName ExecutionFunction;
+	int32 OutputLink;
+	FWeakObjectPtr CallbackTarget;
+
+	const ERyTickUntilStopInExecs* InExecs;
+	ERyTickUntilStopOutExecs* OutExecs;
+
+	FRyTickUntilStopAction(FLatentActionInfo const& latentInfo, const ERyTickUntilStopInExecs* inExecs, ERyTickUntilStopOutExecs* outExecs)
+		: ExecutionFunction(latentInfo.ExecutionFunction)
+		, OutputLink(latentInfo.Linkage)
+		, CallbackTarget(latentInfo.CallbackTarget)
+		, InExecs(inExecs)
+		, OutExecs(outExecs)
+	{
+	}
+
+	virtual void UpdateOperation(FLatentResponse& Response) override
+	{
+		if(*InExecs == ERyTickUntilStopInExecs::Start)
+		{
+			*OutExecs = ERyTickUntilStopOutExecs::Tick;
+			Response.TriggerLink(ExecutionFunction, OutputLink, CallbackTarget);
+		}
+		else
+		{
+			*OutExecs = ERyTickUntilStopOutExecs::Stopped;
+			Response.FinishAndTriggerIf(true, ExecutionFunction, OutputLink, CallbackTarget);
+		}
+	}
+};
+
+//---------------------------------------------------------------------------------------------------------------------
+/**
+*/
+void URyRuntimeWorldHelpers::TickUntilStop(const UObject* WorldContextObject, const ERyTickUntilStopInExecs& InExecs, ERyTickUntilStopOutExecs& OutExecs, FLatentActionInfo LatentInfo)
+{
+	if (UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::LogAndReturnNull))
+	{
+		FLatentActionManager& LatentActionManager = World->GetLatentActionManager();
+		if (LatentActionManager.FindExistingAction<FRyTickUntilStopAction>(LatentInfo.CallbackTarget, LatentInfo.UUID) == nullptr && InExecs == ERyTickUntilStopInExecs::Start)
+		{
+			LatentActionManager.AddNewAction(LatentInfo.CallbackTarget, LatentInfo.UUID, new FRyTickUntilStopAction(LatentInfo, &InExecs, &OutExecs));
+		}
+	}
+}
