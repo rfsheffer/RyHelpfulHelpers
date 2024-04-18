7db3f3ec4fefc99d4c0f522323361756d6cae1e1
rfsheffer <rfsheffer@gmail.com>
2024-04-18 11:19:07 -0700

    - Started editor texture helpers with a function to change max size

---
+++ Source/RyEditor/Private/RyEditorTextureHelpers.cpp
@@ -0,0 +1,17 @@
+﻿// Copyright 2012-2024 Interdimensional Games, Inc. All Rights Reserved.
+
+
+#include "RyEditorTextureHelpers.h"
+
+//---------------------------------------------------------------------------------------------------------------------
+/**
+*/
+void URyEditorTextureHelpers::SetTextureMaximumTextureSize(UTexture2D* texture, const int32 newMaxSize)
+{
+	if(texture)
+	{
+		texture->MaxTextureSize = newMaxSize;
+		texture->MarkPackageDirty();
+		texture->PostEditChange();
+	}
+}
---
+++ Source/RyEditor/Public/RyEditorTextureHelpers.h
@@ -0,0 +1,20 @@
+﻿// Copyright 2012-2024 Interdimensional Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "RyEditorTextureHelpers.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RYEDITOR_API URyEditorTextureHelpers : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+	/** Set the maximum resolution for generated textures. A value of 0 means the maximum size for the format on each platform. */
+	UFUNCTION(BlueprintCallable, Category = "RyRuntime|TextureHelpers")
+	static void SetTextureMaximumTextureSize(class UTexture2D* texture, const int32 newMaxSize);
+};
